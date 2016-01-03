038c61e5a0a409102bfb30ae46a00a11bba7bffa
Leonie <wolfgangreif@MacBook-Pro.fritz.box>
2016-01-03 14:36:41 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<small class="themeinfo">
+    <?php echo __('Powered by WordPress', 'leotheme'); ?>
+    | <a href="sheinit.com/impressum">Impressum</a>
+</small>
+<script>
+jQuery( document ).ready( function( $ ) {
+    $( '#container2' ).masonry( { columnWidth: 220 } );
+} );
+</script>
+<?php wp_footer(); ?>
+</body>
+</html>
