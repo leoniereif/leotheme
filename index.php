d6461c4f73e04925a583d577e3244daac02bb732
Leonie <leonie.reif@gatech.edu>
2016-01-04 18:36:49 +0100

    changed html ids

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <?php include_once( 'header.php' ); ?>
-<div id="container2">
+<div id="container">
     <?php
     $temp = $wp_query;
     $wp_query= null;
@@ -9,12 +9,12 @@
     $wp_query->query('posts_per_page=100'.'&paged='.$paged);
     while ($wp_query->have_posts()) : $wp_query->the_post();
     ?>
-    <div class="brick">
-        <div class="brick_header">
+    <div class="block">
+        <div class="block_header">
             <a href="<?php the_permalink() ?>" rel="bookmark" title="Click to view: <?php the_title_attribute(); ?>">
                 <?php the_title(); ?></a>
             </div>
-            <div class="brick_featured_image">
+            <div class="block_featured_image">
                 <?php if ( has_post_thumbnail() ) {
                     $size=75;
                     ?>
@@ -23,8 +23,8 @@
                     </a>
                 </div>
                 <?php the_excerpt(); ?>
-                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" class="read_more_link"></a>
-            </div>
+                <!--<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" class="read_more_link"></a>
+            </div>-->
         <?php endwhile;?>
         <?php $wp_query = null; $wp_query = $temp;?>
     </div>
